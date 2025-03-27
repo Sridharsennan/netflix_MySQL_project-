4ade06f237644eff874a9fef21b0d4fd8656a3ba
Sridhar Sennan <sridharsennan10@gmail.com>
2025-03-27 18:34:48 +0530

    Create Business Problems netflix.sql

---
+++ Business Problems netflix.sql
@@ -0,0 +1,13 @@
+1. Count the number of movies vs TV shows
+2. Find the most common rating for movies and TV shows
+3. List all movies released in a specific year (e.g., 2020)
+4. Find the top 5 countries with the most content on Netflix
+5. Identify the longest movie or TV show duration
+6. Identify the longest movie
+7. Find content added in the last 5 years
+8. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+9. List all TV shows with more than 5 seasons
+10.Find each year and the average numbers of content release in India on netflix andreturn top 5 year with highest avg content release!
+11. List all movies that are documentaries
+12. Find all content without a director
+13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
